0db42570b100ca0e4dfc7210fe122c4357b6741c
Blancotest8k <119964986+Blancotest8k@users.noreply.github.com>
2025-07-09 17:42:09 +0100

    con

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 const nextConfig = {
   output: 'export',
   distDir: './dist',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   eslint: {
     ignoreDuringBuilds: true, // Skip ESLint during build
   },
